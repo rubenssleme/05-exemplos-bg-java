27af067ad27b4c3e0f58872b9a24f21f871c1101
rubens leme <rubenss.leme@gmail.com>
2019-03-13 12:40:49 -0300

    Ajuste de branche

---
+++ meuCarro_A/src/meuCarro_A/Principal.java
@@ -0,0 +1,14 @@
+package meuCarro_A;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		//Instanciar objeto
+		Carro fucaBala = new Carro();
+		fucaBala.setLigado(true);
+		fucaBala.ligarCarro();
+
+
+	}
+
+}
